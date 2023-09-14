3af3ac48411914f667229fb517632bc19b006adc
Adrian Kujawski <adriankujawski313@gmail.com>
2023-09-14 21:46:53 +0200

    getStream

--- src/Server.cs
+++ src/Server.cs
@@ -2,85 +2,99 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-Redis redis = new(new(IPAddress.Loopback, 6379));
-redis.Start();
-using (Socket socket = redis.AcceptSocket())
+TcpListener server = new(IPAddress.Any, 6379);
+server.Start();
+using TcpClient client = server.AcceptTcpClient();
+byte[] bytes = new byte[256];
+while (true)
 {
-    while (true)
+    NetworkStream clientStream = client.GetStream();
+    while (clientStream.Read(bytes, 0, bytes.Length) != 0)
     {
-        redis.HandleMessage(socket);
+        Span<byte> msg = Encoding.ASCII.GetBytes("+PONG\r\n");
+        clientStream.Write(msg);
     }
 }
 
+// Redis redis = new(new(IPAddress.Loopback, 6379));
+// redis.Start();
+// using (Socket socket = redis.AcceptSocket())
+// {
+//     while (true)
+//     {
+//         redis.HandleMessage(socket);
+//     }
+// }
 
-class Redis
-{
-    private readonly TcpListener server;
-    private readonly byte[] data = new byte[1024];
 
-    public Redis(TcpListener server) => this.server = server;
+// class Redis
+// {
+//     private readonly TcpListener server;
+//     private readonly byte[] data = new byte[1024];
 
-    public void Start()
-    {
-        server.Start();
-        Console.WriteLine("Server has started on {0}.", server.LocalEndpoint);
-    }
+//     public Redis(TcpListener server) => this.server = server;
 
-    internal void HandleMessage(Socket? socket)
-    {
-        if (socket == null) return;
-        var message = GetMessage(socket);
-        // if (IsPing(message))
-        {
-            PongResposne(socket, message!);
-        }
-    }
+//     public void Start()
+//     {
+//         server.Start();
+//         Console.WriteLine("Server has started on {0}.", server.LocalEndpoint);
+//     }
 
-    static bool IsPing(string? message)
-    {
-        Console.WriteLine($"IsPing: {message?.StartsWith(Message.ping) == true}");
+//     internal void HandleMessage(Socket? socket)
+//     {
+//         if (socket == null) return;
+//         var message = GetMessage(socket);
+//         // if (IsPing(message))
+//         {
+//             PongResposne(socket, message!);
+//         }
+//     }
 
-        return message?.StartsWith(Message.ping) == true;
-    }
+//     static bool IsPing(string? message)
+//     {
+//         Console.WriteLine($"IsPing: {message?.StartsWith(Message.ping) == true}");
 
-    private string? GetMessage(Socket socket)
-    {
-        if (socket.Available == 0) return null;
-        var receivedBytes = socket.Receive(data);
-        if (receivedBytes == 0) return null;
+//         return message?.StartsWith(Message.ping) == true;
+//     }
 
-        var receivedMessaged = Encoding.ASCII.GetString(data, 0, receivedBytes);
-        Console.WriteLine($"receivedMessaged: {receivedMessaged}");
-        return receivedMessaged;
-    }
+//     private string? GetMessage(Socket socket)
+//     {
+//         if (socket.Available == 0) return null;
+//         var receivedBytes = socket.Receive(data);
+//         if (receivedBytes == 0) return null;
 
-    private void PongResposne(Socket socket, string recivedMessage)
-    {
-        // var response = "+PONG\r\n";
-        // var elapsedMessage = recivedMessage.Replace(Message.ping, string.Empty);
-        // Console.WriteLine($"elapsedMessage: {elapsedMessage}");
-        // if (elapsedMessage?.Length > 0)
-        // {
-        //     response = elapsedMessage;
-        // }
-        // Console.WriteLine(response);
-        socket.Send(Message.Serialize(Message.pong), SocketFlags.None);
-    }
+//         var receivedMessaged = Encoding.ASCII.GetString(data, 0, receivedBytes);
+//         Console.WriteLine($"receivedMessaged: {receivedMessaged}");
+//         return receivedMessaged;
+//     }
 
-    internal Socket AcceptSocket()
-    {
-        return server.AcceptSocket();
-    }
-}
+//     private void PongResposne(Socket socket, string recivedMessage)
+//     {
+//         // var response = "+PONG\r\n";
+//         // var elapsedMessage = recivedMessage.Replace(Message.ping, string.Empty);
+//         // Console.WriteLine($"elapsedMessage: {elapsedMessage}");
+//         // if (elapsedMessage?.Length > 0)
+//         // {
+//         //     response = elapsedMessage;
+//         // }
+//         // Console.WriteLine(response);
+//         socket.Send(Message.Serialize(Message.pong), SocketFlags.None);
+//     }
 
-class Message
-{
-    public const string pong = "PONG";
-    public const string ping = "ping";
-    public const string endMessage = "\r\n";
-    public const string stringType = "+";
-    public static byte[] Serialize(string value)
-    {
-        return Encoding.UTF8.GetBytes($"{stringType}{value}{endMessage}");
-    }
-}
+//     internal Socket AcceptSocket()
+//     {
+//         return server.AcceptSocket();
+//     }
+// }
+
+// class Message
+// {
+//     public const string pong = "PONG";
+//     public const string ping = "ping";
+//     public const string endMessage = "\r\n";
+//     public const string stringType = "+";
+//     public static byte[] Serialize(string value)
+//     {
+//         return Encoding.UTF8.GetBytes($"{stringType}{value}{endMessage}");
+//     }
+// }
